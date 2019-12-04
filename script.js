97628ac21f6d4b939edbcc108c533f191561cef8
Ariana Freitag <freitagariana@gmail.com>
2019-12-04 14:30:22 -0500

    init

---
+++ script.js
@@ -0,0 +1,40 @@
+function togglePopup() {
+  var popup = document.getElementById("popupContainer");
+  popup.classList.toggle("show");
+}
+
+function toggleSecondPopup() {
+  var redpopup = document.getElementById("infographic");
+  redpopup.classList.toggle("show");
+}
+
+function toggleThirdPopup() {
+  var thirdpopup = document.getElementById("infographic2");
+  thirdpopup.classList.toggle("show");
+}
+
+function toggleAshPopup() {
+  var AshPopup = document.getElementById("ashPopuptext")
+  AshPopup.classList.toggle("show")
+}
+
+function toggleGasPopup() {
+  var GasPopup = document.getElementById("gasPopuptext")
+  GasPopup.classList.toggle("show")
+}
+
+function toggleCornPopup() {
+  var CornPopup = document.getElementById("cornPopuptext")
+  CornPopup.classList.toggle("show")
+}
+
+function toggleSquigglePopup() {
+  var SquigglePopup = document.getElementById("squigglePopuptext")
+  SquigglePopup.classList.toggle("show")
+}
+
+function toggleBeakerPopup() {
+  var BeakerPopup = document.getElementById("beakerPopuptext")
+  BeakerPopup.classList.toggle("show")
+}
+
